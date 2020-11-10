595d749cd95f4ade18f57b33baa654475f2e4f5f
cjcordosi <cjc6312@psu.edu>
2020-11-10 11:39:33 -0500

    Commented main Mine Tracker file to define master repo.

--- src/me/frontside/minetracker/MineTracker.java
+++ src/me/frontside/minetracker/MineTracker.java
@@ -2,7 +2,7 @@ package me.frontside.minetracker;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-
+// MASTER MineTracker Repo
 public class MineTracker extends JavaPlugin {
 
     private static MineTracker plugin;
